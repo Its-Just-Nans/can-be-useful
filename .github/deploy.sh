93d2c8f4ed085521b53febc59361cf732d30033f
n4n5 <its.just.n4n5@gmail.com>
2025-09-10 13:10:56 -0600

    add link

--- .github/deploy.sh
+++ .github/deploy.sh
@@ -7,5 +7,5 @@ cp -R cdn dist/
 
 python -m pip install -U indexage
 cd dist/ || exit 1
-python -m indexage . --skip-exists
+python -m indexage . --skip-exists --link https://github.com/Its-Just-Nans/can-be-useful/tree/main/
 cd ..|| exit 1
