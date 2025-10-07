c63b703dff0a1e80e8724755607ee6f84d28a698
n4n5 <its.just.n4n5@gmail.com>
2025-10-06 19:22:30 -0600

    update deploy

--- .github/deploy.sh
+++ .github/deploy.sh
@@ -3,7 +3,26 @@
 rm -rf dist/
 mkdir -p dist/
 cp -R html-* dist/
+cp -R js-onclick-interceptor dist/
 cp -R cdn dist/
+
+projects=("svelte-bad-font-viewer" "svelte-cv-creator")
+
+output_folders=("dist" "public")
+
+# Loop through each element in the array
+for project in "${projects[@]}"; do
+    echo "building $project"
+    pushd "$project" || exit 1
+    npm install
+    npm run build
+    for output_folder in "${output_folders[@]}"; do
+        if [ -e "$output_folder" ]; then
+            cp -R "$output_folder/" "../dist/$project"
+        fi
+    done
+    popd || exit 1
+done
 
 python -m pip install -U indexage
 cd dist/ || exit 1
