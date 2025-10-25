4c5feb879529062ce7fb85fc8efb80706e8981ea
n4n5 <its.just.n4n5@gmail.com>
2025-10-25 14:41:01 -0600

    mv from https://github.com/Its-Just-Nans/lab

---
+++ svelte-time-based-animation/src/App.svelte
@@ -0,0 +1,11 @@
+<script>
+    import TimeBasedAnimation from "./lib/TimeBasedAnimation.svelte";
+</script>
+
+<TimeBasedAnimation />
+
+<style>
+    :global(body) {
+        margin: 0px;
+    }
+</style>
