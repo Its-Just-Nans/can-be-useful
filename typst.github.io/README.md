0f79e5c0855c049e586c3ff6991cae73376e2128
n4n5 <its.just.n4n5@gmail.com>
2025-11-29 19:47:42 -0700

    mv from https://github.com/Its-Just-Nans/typst.github.io

---
+++ typst.github.io/README.md
@@ -0,0 +1,17 @@
+# typst.github.io
+
+- https://github.com/typst-jp/typst-jp.github.io/issues/233
+- https://github.com/typst/webapp-issues/issues/449
+- https://forum.typst.app/t/guide-preview-typst-app-docs-of-your-pr-on-netlify/5291
+- https://github.com/typst-jp/typst-jp.github.io
+
+It is slow? Absolutely
+
+It is useful? I guess it could
+
+## Known bugs
+
+- Banner for Japanese translations error
+- Button to come at the home page is redirecting to "/docs"
+- Some text still in japanese
+- Search bar not working for now
---
+++ typst.github.io/deploy.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+set -x
+
+if [ ! -d "typst" ]; then
+    echo "Typst directory does not exist. Cloning repository..."
+    git clone https://github.com/typst/typst.git
+fi
+if [ ! -d "typst/assets" ]; then
+    cd typst || exit 1
+    cargo run --package typst-docs -- --assets-dir assets/docs --out-file assets/docs.json --base /en/
+    cd ..
+fi
+
+
+if [ ! -d "typst-jp.github.io" ]; then
+    echo "Website directory does not exist. Cloning repository..."
+    git clone https://github.com/typst-jp/typst-jp.github.io.git
+fi
+if [ ! -d "typst-jp.github.io/assets" ]; then
+    cd typst-jp.github.io || exit 1
+    cargo run --package typst-docs -- --assets-dir assets/docs --out-file assets/docs.json --base /jp/
+    cd ..
+fi
+
+rm -rf dist/
+mkdir -p dist/
+
+if [ -d "website_builder" ]; then
+    rm -rf website_builder
+fi
+
+mkdir -p website_builder
+cp -r typst-jp.github.io/website website_builder/
+cd website_builder/website || exit 1
+npm install
+rm -rf public/index.html
+# delete the js redirection to /docs
+sed -i '29,31d' src/index.tsx
+sed -i 's|https://typst-jp.github.io/|https://typst-github-io.n4n5.dev/|g' vite.config.ts
+cd ../..
+
+cp -r typst/assets/ website_builder/assets/
+cd website_builder/website || exit 1
+sed -i '9s|docs/||' package.json
+sed -i '9s|dist/|dist/en/|' package.json
+sed -i 's|/pagefind/pagefind|/en/pagefind/pagefind|g' src/components/ui/common/SearchWindow.tsx
+npm run build
+mv dist/ ../../dist/en/
+cd ../..
+# sleep 20
+rm -rf website_builder/assets/
+cp -r typst-jp.github.io/assets/ website_builder/assets/
+cd website_builder/website || exit 1
+sed -i '9s|dist/en/|dist/jp/|' package.json
+sed -i 's|/en/pagefind/pagefind|/jp/pagefind/pagefind|g' src/components/ui/common/SearchWindow.tsx
+npm run build
+mv dist/ ../../dist/jp/
+cd ../..
+cp index.html dist/
+cp -r dist/jp/assets/ dist/assets
+mv dist/en/en/* dist/en/
+mv dist/en/assets/docs/* dist/en/assets/
+mv dist/jp/jp/* dist/jp/
+mv dist/jp/assets/docs/* dist/jp/assets/
