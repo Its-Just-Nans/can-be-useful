bb7096ee001d631aeb881062e130198b937b3d4d
n4n5 <its.just.n4n5@gmail.com>
2026-01-03 16:11:23 -0700

    mv from https://github.com/Its-Just-Nans/coreutils-tracking-details

---
+++ python-coreutils-tracking-details/README.md
@@ -0,0 +1,7 @@
+# coreutils-tracking-details
+
+- <https://github.com/uutils>
+- <https://github.com/uutils/coreutils>
+- <https://github.com/uutils/coreutils-tracking>
+
+> Made between 2024-06-28 and 2024-11-05
---
+++ python-coreutils-tracking-details/builder.py
@@ -0,0 +1,200 @@
+"""builder https://github.com/Its-Just-Nans/coreutils-tracking-details"""
+
+from datetime import date
+import json
+from pathlib import Path
+import re
+from os import makedirs
+
+
+START_INDEX = """
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Test Results</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+        }
+        table {
+            width: 100%;
+            border-collapse: collapse;
+        }
+        th, td {
+            padding: 8px 12px;
+            border: 1px solid #ccc;
+            text-align: left;
+        }
+        th {
+            background-color: #f4f4f4;
+        }
+        .pass {
+            background-color: #d4edda;
+        }
+        .fail {
+            background-color: #f8d7da;
+        }
+        .skip {
+            background-color: #fff3cd;
+        }
+        #all:checked ~ table tr {
+            display: table-row;
+        }
+        #pass:checked ~ table tr.fail, #pass:checked ~ table tr.skip {
+            display: none;
+        }
+        #fail:checked ~ table tr.pass, #fail:checked ~ table tr.skip {
+            display: none;
+        }
+        #skip:checked ~ table tr.pass, #skip:checked ~ table tr.fail {
+            display: none;
+        }
+        pre{
+            background-color: lightgray;
+            padding: 10px;
+            overflow: auto;
+        }
+    </style>
+</head>
+<body>
+"""
+
+END_INDEX = """
+</body>
+</html>
+"""
+
+
+def get_html_content(passed, failed, skipped):
+    """Generate the HTML content for the test results"""
+    total = passed + failed + skipped
+    today = date.today()
+    return f"""
+    {START_INDEX}
+    <h1>Test Results - last updated {today}</h1>
+    <input type="radio" id="all" name="show" />
+    <label for="all">All ({total})</label>
+    <input type="radio" id="pass" name="show" />
+    <label for="pass">Pass ({passed})</label>
+    <input type="radio" id="fail" name="show" checked/>
+    <label for="fail">Fail ({failed})</label>
+    <input type="radio" id="skip" name="show" />
+    <label for="skip">Skip ({skipped})</label>
+    <table>
+        <thead>
+            <tr>
+                <th>Category</th>
+                <th>Log File</th>
+                <th>Status</th>
+            </tr>
+        </thead>
+        <tbody>
+"""
+
+
+# Function to generate the HTML content
+def generate_html(data):
+    """Generate the HTML content for the test results"""
+    passed, failed, skipped = 0, 0, 0
+    html_content = ""
+    for category, logs in data.items():
+        for log_file, status in logs.items():
+            status_class = status.lower()  # use status as class for styling
+            if status == "PASS":
+                passed += 1
+            elif status == "FAIL":
+                failed += 1
+            elif status == "SKIP":
+                skipped += 1
+            name = (
+                f"<a href='{log_file}.html'>{log_file}</a>"
+                if status != "PASS"
+                else log_file
+            )
+            html_content += f"""
+            <tr class="{status_class}">
+                <td>{category}</td>
+                <td>
+                    {name}
+                </td>
+                <td>{status}</td>
+            </tr>
+"""
+    html_content += f"""
+        </tbody>
+    </table>
+{END_INDEX}
+"""
+    html_content = get_html_content(passed, failed, skipped) + html_content
+    return html_content
+
+
+def decode_logs_file():
+    """Decode the logs file and return a dictionary with the results"""
+    results = {}
+    for one_file in ["test-suite.log", "test-suite-root.log"]:
+        with open(one_file, "rb") as f:
+            lines = f.read()
+
+        lines = lines.decode("utf-8", "ignore")
+        for regex in [r"^SKIP: .*\n=*(.|\n)*?SKIP .*", r"^FAIL: .*\n=*(.|\n)*?FAIL .*"]:
+            matches = re.finditer(regex, lines, re.MULTILINE)
+            for _, match in enumerate(matches, start=1):
+                one_res = match.group()
+                res = one_res.split("\n")
+                test_file = res[-1].split(" ")[1]
+                test_name = res[0].split("/")[-1]
+                text = "\n".join(res)
+                num = re.findall(r"failed:", text)
+                results[f"{test_name}.log"] = (text, test_file, len(num))
+
+    return results
+
+
+def improve_text_out(text_out):
+    """Improve the text output"""
+    rgx = re.compile(r"^(.*?\.\.\.)", re.MULTILINE)
+    text_out = "\n".join(
+        [
+            re.sub(rgx, "\n\n\g<1>", text_part_out)
+            for text_part_out in text_out.split("\n")
+        ]
+    )
+    return text_out
+
+
+def html_test_output(name, one_output):
+    """Generate the HTML output for a test"""
+    today = date.today()
+    text_out, test_file, res = one_output
+    text_out_cleaned = improve_text_out(text_out)
+    res_print = f"{res} " if res != 0 else ""
+    txt = START_INDEX
+    txt += f"<h2>{name} ({res_print}failed) - last updated {today}</h2>\n"
+    link = f"https://github.com/coreutils/coreutils/blob/master/{test_file}"
+    txt += f"<a href='{link}' target='_blank'>test link: {test_file}</a>"
+    txt += f"<pre>{text_out_cleaned}</pre>"
+    txt += END_INDEX
+    output_path = Path(f"dist/{name}.html")
+    output_path.write_text(txt, encoding="utf-8")
+
+
+def main():
+    """Main function"""
+    makedirs("dist", exist_ok=True)
+    json_data = json.loads(Path("gnu-full-result.json").read_text(encoding="utf-8"))
+    html_content = generate_html(json_data)
+
+    # Write the HTML content to an index.html file
+    output_path = Path("dist").joinpath("index.html")
+    output_path.write_text(html_content, encoding="utf-8")
+    tables = decode_logs_file()
+    for test_name, logs in tables.items():
+        # print(test_name)
+        html_test_output(test_name, logs)
+
+
+if __name__ == "__main__":
+    main()
