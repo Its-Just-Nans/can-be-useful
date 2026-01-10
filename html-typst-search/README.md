0462ece6d8bb58579dd12095e8ad220171399cbe
n4n5 <its.just.n4n5@gmail.com>
2026-01-10 16:56:27 -0700

    add README

---
+++ html-typst-search/README.md
@@ -0,0 +1,15 @@
+# html-typst-search
+
+Using <https://github.com/nextapps-de/flexsearch>
+
+```sh
+build.sh
+# will clone typst
+# generate the docs as data.json
+# then create a very simple index from the data.json
+# then do deployement stuff
+```
+
+## Related to
+
+- <https://github.com/typst/webapp-issues/issues/449#issuecomment-3733696572>
--- html-typst-search/build.sh
+++ html-typst-search/build.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
-if [ ! -d "typst" ]; then
-    git clone https://github.com/typst/typst.git
-fi
+
 if [ ! -f "data.json" ];then
+    if [ ! -d "typst" ]; then
+        git clone https://github.com/typst/typst.git
+    fi
     cd typst/docs || exit 1
     cargo run -- --out-file data.json
     cp data.json ../..
     cd ../..
 fi
+
+# create index
 node index.js
+
+# deployement stuff
 curl "https://cdn.jsdelivr.net/gh/nextapps-de/flexsearch@0.8.2/dist/flexsearch.light.module.min.js" > flexsearch.light.module.min.js
 
 rm -rf dist/
