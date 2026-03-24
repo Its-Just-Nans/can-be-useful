da456cf398b661b93a1b240b95a8a196c5cb1fb2
n4n5 <git@n4n5.dev>
2026-03-24 10:25:14 -0600

    Update main.py

--- python-crate-list/main.py
+++ python-crate-list/main.py
@@ -29,6 +29,30 @@ def extract_github_repo(repo_url):
         return f"{parts[0]}/{repo}"
     return None
 
+def get_version_color(version: str) -> str:
+    default_color = "yellow"
+    try:
+        major = int(version.split('.')[0])
+    except (ValueError, IndexError):
+        return default_color
+
+    colors = {
+        0: "orange",
+        1: "blue",
+        2: "green",
+        3: "red",
+        4: "purple",
+        5: "pink",
+        6: "cyan",
+        7: "magenta",
+        8: "teal",
+        9: "brown",
+    }
+
+    if major > 10:
+        return "yellow"
+
+    return colors.get(major, default_color)
 
 def fetch_crates():
     """fetch crates"""
@@ -67,22 +91,25 @@ def generate_html(crates):
     for crate in crates:
         repo_url = crate.get("repository")
         crate_name = crate.get("name")
+        crate_version = crate.get("default_version")
         github_repo = extract_github_repo(repo_url)
+        version_color = get_version_color(crate_version)
 
         if github_repo:
             badge_url = f"https://img.shields.io/github/stars/{github_repo}"
-            badge_version_url = f"https://img.shields.io/crates/v/{crate_name}"
         else:
-            badge_url = ""
             badge_version_url = ""
+        badge_version_url = f"https://img.shields.io/crates/v/{crate_name}"
+        
         html_lines.append(f"""
             <tr>
                 <td>
-                    {crate["name"]}
+                    <a href="https://crates.io/crates/{crate_name}">{crate_name}</a>
                 </td>
                 <td>
                     <a href="{repo_url}">{repo_url}</a>
                 </td>
+                <td style="background-color: {version_color}">{crate_version}<td/>
                 <td>
                     <img src="{badge_url}" />
                 </td>
