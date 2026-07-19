9f58ebd79bbe7bb2215487bf0eefaa1e21328b48
n4n5 <its.just.n4n5@gmail.com>
2026-07-19 20:49:10 +0200

    curl trunk

--- rust-pdf_to_text/build.sh
+++ rust-pdf_to_text/build.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-cargo install trunk --locked
-trunk build --release --public-url ./
+curl -L -o trunk-x86_64-unknown-linux-gnu.tar.gz \
+https://github.com/trunk-rs/trunk/releases/latest/download/trunk-x86_64-unknown-linux-gnu.tar.gz
+
+tar -xzf trunk-x86_64-unknown-linux-gnu.tar.gz
+chmod +x trunk
+
+./trunk build --release --public-url ./
