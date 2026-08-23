e5c9876ef1fe4c53893bd263aba6d071418c9dbb
n4n5 <its.just.n4n5@gmail.com>
2026-08-23 22:32:44 +0200

    fix: rewrite URLs

--- astro-sportiva/astro.config.mjs
+++ astro-sportiva/astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://n4n5.dev/can-be-useful/astro-sportiva/",
-    base: "https://n4n5.dev/can-be-useful/astro-sportiva/",
+    site: "https://n4n5.dev",
+    base: "/can-be-useful/astro-sportiva/",
+    trailingSlash: "always"
 });
