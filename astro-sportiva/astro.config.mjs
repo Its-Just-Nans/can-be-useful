8ff9485aeefe1d402c2250a90945ed7433639837
n4n5 <its.just.n4n5@gmail.com>
2026-08-23 22:07:32 +0200

    add base

--- astro-sportiva/astro.config.mjs
+++ astro-sportiva/astro.config.mjs
@@ -2,5 +2,6 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-    site: `https://n4n5.dev/can-be-useful/astro-sportiva/`,
+    site: "https://n4n5.dev/can-be-useful/astro-sportiva/",
+    base: "https://n4n5.dev/can-be-useful/astro-sportiva/",
 });
